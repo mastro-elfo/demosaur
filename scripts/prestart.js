47e989e73b45db5836475a1991d3ffc71aa23640
mastro-elfo <francesco.209@gmail.com>
2020-10-27 12:51:31 +0100

    New script

---
+++ scripts/prestart.js
@@ -0,0 +1,9 @@
+const package = require("../package.json");
+const fs = require("fs");
+
+// Output version to 'src/version.js'
+
+fs.writeFileSync(
+  "./src/version.js",
+  `export const version = "${package.version}";`
+);
